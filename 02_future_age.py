b363b91d5f23a8d0805d4d049fe059b7da97b468
Keya Vadgama <keyaunstopped@gmail.com>
2026-08-15 14:29:23 +0530

    future age calculation based on user input

---
+++ 02_future_age.py
@@ -0,0 +1,7 @@
+age=int(input("Enter you age here: "))
+age=age+5
+print("Your age after 5 years will be",age)
+age=age+5
+print("Your age after 10 years will be",age)
+age=age+10
+print("Your age after 20 years will be",age)
