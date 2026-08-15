4f680e93b41db4150db12b8f19bed57430eeaff0
Keya Vadgama <keyaunstopped@gmail.com>
2026-08-15 19:40:12 +0530

    Shopping receipt calculation script

---
+++ 07_shopping_receipt.py
@@ -0,0 +1,14 @@
+p1=int(input("Price of Item 1: "))
+q1=int(input("Quantity of Item 1: "))
+p2=int(input("Price of Item 2: "))
+q2=int(input("Quantity of Item 2: "))
+p3=int(input("Price of Item 3: "))
+q3=int(input("Quantity of Item 3: "))
+tl1=p1*q1
+print("Total amount of first product",tl1)
+tl2=p2*q2
+print("Total amount of second product",tl2)
+tl3=p3*q3
+print("Total amount of third product",tl3)
+gd=tl1+tl2+tl3
+print("Total amount of the the all the purchase is",gd)
