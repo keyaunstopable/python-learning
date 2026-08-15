cb0120b63ca480584ee330cbbd6053c45cd6cf05
Keya Vadgama <keyaunstopped@gmail.com>
2026-08-15 19:56:58 +0530

    Add functionality to process two-digit numbers

---
+++ 08_two_digit_number.py
@@ -0,0 +1,7 @@
+num=int(input("Enter your fav 2 digit number: "))
+tens_digit = num // 10
+ones_digit = num % 10
+print("Tens digit: ",tens_digit )
+print("Units digit: ",ones_digit )
+print("The sum of the two digits: ",tens_digit + ones_digit )
+print("The product of the digits: ",tens_digit * ones_digit )
